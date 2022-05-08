aefdf48f0fbf7592c23d98c9bd10e10471959d49
Svyat <svyat.kovtun@gmail.com>
2022-05-08 20:26:46 +0300

    feat: homepage

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import './styles.scss'
+
+const Home = () => (
+  <div className="home">
+    <div className="filter" />
+    <div className="content">
+
+      <h1 className="title">Svyat Kovtun</h1>
+      <h2 className="text">React.JS Engineer @ <a href="https://crowdbotics.com/" target="_blank" rel="noreferrer">Crowdbotics</a></h2>
+
+      <div className="emoji">🐱‍👤</div>
+
+      <p className="text text--sm">Reach me: <a href="mailto:svyat.kovtun@gmail.com?subject=Hello!">svyat.kovtun@gmail.com</a></p>
+    </div>
+  </div>
+)
+
+export default Home
