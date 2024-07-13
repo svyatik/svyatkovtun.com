44f1930c04147c83208dde12a5eef49e2e8523cc
svyatik <svyat.kovtun@gmail.com>
2024-07-14 00:42:35 +0300

    feat: improvements

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,35 @@
 import ThemeTogglerButton from '../../components/ThemeToggler'
+import { ThemeContext } from '../../contexts/theme-context'
 
-// import Ninja from '../../img/ninja.svg'
+import Ninja from '../../img/ninja.svg'
+import Github from '../../assets/Github'
+
 import './styles.scss'
 
 const Home = () => (
-  <div className="home">
-    <div className="filter" />
-    <div className="content">
-      <h1 className="title">Svyat Kovtun</h1>
-      <h2 className="text">React.JS Engineer <span className="nowrap">@ <a href="https://crowdbotics.com/" target="_blank" rel="noreferrer">Crowdbotics</a></span></h2>
+  <ThemeContext.Consumer>
+    {({ theme }) => (
+      <div className="home">
+        <div className="filter" />
 
-      {/* <img className="emoji" src={Ninja} width="156" alt="🐱‍👤" /> */}
+        <div className="content">
+          <h1 className="title">Svyat Kovtun</h1>
+          <h2 className="text">React.JS Engineer <span className="nowrap">@ <a href="https://crowdbotics.com/" target="_blank" rel="noreferrer">Crowdbotics</a></span></h2>
 
-      <p className="text text--sm">Reach me: <a href="mailto:svyat.kovtun@gmail.com?subject=Hello!">svyat.kovtun@gmail.com</a></p>
+          <img className="emoji" src={Ninja} width="156" alt="🐱‍👤" />
+          <p className="text text--sm">Reach me: <a href="mailto:svyat.kovtun@gmail.com?subject=Hello!">svyat.kovtun@gmail.com</a></p>
+        </div>
 
-      <ThemeTogglerButton />
-    </div>
-  </div>
+        <div className="header">
+          <ThemeTogglerButton />
+
+          <a className="header__link" title="GitHub" href="https://github.com/svyatik" target="_blank" rel="noreferrer">
+            <Github color={theme === 'dark' ? '#fff' : '#000'} width={24} />
+          </a>
+        </div>
+      </div>
+  )}
+  </ThemeContext.Consumer>
 )
 
 export default Home
